30cc7c376b80d0a6d9e65020ddeae3b8d59b8be3
Devdazz <>
2022-08-02 14:50:01 +0000

    input checking & feedback message implemented

--- Main.java
+++ Main.java
@@ -14,8 +14,24 @@ class Main {
     do {
       System.out.println("Guess my number (enter -1 to exit)");
       userNumber = sc.nextInt();
+
+      // check if user guessed right 
+         if (userNumber == myNumber) {
+        System.out.println("Woohoo .. correct number");
+        break; //end the game if user is right
+      }
+        //check if number is greater than userNumber
+         else if (userNumber > myNumber) {
+        System.out.println("your number is too large");
+      } 
+        //check if number is lesser than userNumber
+         else {
+        System.out.println("your number is too small");
+      }
    
     } while (userNumber >= 0); //exit the loop for -ve number 
+    
+    // print the random was picked
     System.out.print("my number was :");
     System.out.println(myNumber);
   }
